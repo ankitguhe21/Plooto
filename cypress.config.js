23c3147bddc7e68a47559c4b73e4e8a585abec59
ankitguhe21 <ankit.guhe22@gmail.com>
2023-04-06 18:13:08 -0400

    added reports and github actions

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,20 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false
+  },
   e2e: {
     baseUrl: 'https://happy-field-011ab7b10.2.azurestaticapps.net',
     watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     }
   }
 });
--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,10 +1,14 @@
 import LoginPage from '../pages/login.page';
 import companySelectionPage from '../pages/company-selection.page';
+
+const password = 'demoPass';
+const userEmail = 'demo@gmail.com';
+
 describe('Login Tests', () => {
   it('Should be able to login with Valid Credentials', () => {
     LoginPage.visitPage;
-    LoginPage.username.type('demo@gmail.com');
-    LoginPage.password.type('demoPass');
+    LoginPage.username.type(userEmail);
+    LoginPage.password.type(password);
     LoginPage.submitButton.click();
     cy.url().should('include', '/company_select.html');
     companySelectionPage.companySelectionTable.should('be.visible');
@@ -13,7 +17,7 @@ describe('Login Tests', () => {
   it('Should not be able to login with empty email', () => {
     cy.log('Failing Intentionally');
     LoginPage.visitPage;
-    LoginPage.username.type('demo@gmail.com');
+    LoginPage.username.type(userEmail);
 
     LoginPage.submitButton.click();
     LoginPage.errorFeild.should('contain', 'Feild is required');
@@ -22,7 +26,7 @@ describe('Login Tests', () => {
   it('Should not be able to login with empty password', () => {
     cy.log('Failing Intentionally');
     LoginPage.visitPage;
-    LoginPage.password.type('demoPass');
+    LoginPage.password.type(password);
 
     LoginPage.submitButton.click();
     LoginPage.errorFeild.should('contain', 'Feild is required');
